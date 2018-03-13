212ab02b0c473849f54a97162f783b76bc26c7c9
Max Kaye <m@xk.io>
2018-03-13 15:50:24 +1100

    manually invoke nvm

--- setup.sh
+++ setup.sh
@@ -15,7 +15,9 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install htop sysstat python3 pyth
 
 # node stuff
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 npm install -g web3 nginx-conf
 
