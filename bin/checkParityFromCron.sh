2b38b3c8b1709be0ad1b9b7631b308464c90e819
Max Kaye <m@xk.io>
2018-03-16 14:56:36 +1100

    update cronjob to help confirm working

--- bin/checkParityFromCron.sh
+++ bin/checkParityFromCron.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 NODE_PATH=`npm root -g` node ~/bin/_cronCheckParity.js && sudo systemctl restart parity
+
+echo $? > ~/.lastParityCheck
+echo "" >> ~/.lastParityCheck
+date +%s >> ~/.lastParityCheck
