4ea3e06ccb9c3a851fe5cf6650a9c0b1079cb82d
Max Kaye <m@xk.io>
2018-04-08 14:08:35 +1000

    fix up script

--- patchOldWithStatsAndEnvVars.sh
+++ patchOldWithStatsAndEnvVars.sh
@@ -10,11 +10,20 @@ echo "Setting node type to $1"
 echo "Continue?"
 select res in Yes No
 do 
-  if [ "$res" = No ]; then; exit; fi
+  if [ "$res" = No ]; then echo 'exiting..'; exit; else break; fi
 done
+
+echo 'proceeding'
 
 echo "export ETH_NETWORK=$1" >> ~/.bashrc
 echo "export NODE_NAME=$(cat /etc/hostname)" >> ~/.bashrc
 
 sed -i 's/"eth", "rpc"/"eth", "net", "rpc"/' ~/.local/share/io.parity.ethereum/config.toml
+
+sudo systemctl restart parity
+
+echo 'installing stats...'
+
+./installBin.sh
+./installStats.sh
 
