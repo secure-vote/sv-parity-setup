1d6a9fdc8902b236d0d1654b380d10c135813867
Max Kaye <m@xk.io>
2018-04-12 14:39:12 +1000

    use sv cron file

--- bin/upgrades/000_init.js
+++ bin/upgrades/000_init.js
@@ -1,6 +1,6 @@
 require('./upgradeLib')("init", function(){
 
-    appendFile("/etc/crontab", write => {
+    appendFile("/etc/cron.d/sv", write => {
         write("0 */12 * * * ubuntu /home/ubuntu/bin/restartPm2.sh");
         write("0 0 * * * ubuntu /home/ubuntu/bin/run_all_upgrades_cron.sh");
     });
