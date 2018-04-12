3df4c49b93b60288e047352f1f1a65345c7ddb96
Max Kaye <m@xk.io>
2018-04-12 14:29:26 +1000

    enable node with sudo?

--- bin/_loadCommon.sh
+++ bin/_loadCommon.sh
@@ -7,3 +7,7 @@ set -e
 . ~/.bashrc
 export NODE_PATH=`npm root -g`
 nvm use default
+
+# a bit hacky...
+sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/node" "/usr/local/bin/node"
+sudo ln -s "$NVM_DIR/versions/node/$(nvm version)/bin/npm" "/usr/local/bin/npm"
