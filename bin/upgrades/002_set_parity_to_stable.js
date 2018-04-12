9751be1f009b066f0598df1b4ba006ce1d0b0443
Max Kaye <m@xk.io>
2018-04-12 15:30:26 +1000

    fix up toml stuff

--- bin/upgrades/002_set_parity_to_stable.js
+++ bin/upgrades/002_set_parity_to_stable.js
@@ -5,6 +5,10 @@ require('./upgradeLib')("002", function(){
     const c = toml.parse(fs.readFileSync(parityConfigPath));
     c['parity']['release_track'] = "stable"
     fs.writeFileSync(parityConfigPath, toml.dump(c))
+    // python toml ends lists like ["item","thing",] -- nodejs toml doesn't like it
+    execCmd("sed -i 's/, undefined//' ~/.local/share/io.parity.ethereum/config.toml")
+    execCmd("sed -i 's/,]/]/' ~/.local/share/io.parity.ethereum/config.toml")
+    execCmd("sed -i 's/, ]/]/' ~/.local/share/io.parity.ethereum/config.toml")
 
     execCmd("systemctl stop parity")
     execCmd("rm -rf ~/.local/share/io.parity.ethereum-updates/");
