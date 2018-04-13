ea52f825844509cd284c7d3b8f0867b28f9b870d
Max Kaye <m@xk.io>
2018-04-13 10:09:52 +1000

    log cron upgrades locally

--- bin/run_all_upgrades_cron.sh
+++ bin/run_all_upgrades_cron.sh
@@ -2,11 +2,11 @@
 
 SLEEP_FOR=$(expr $RANDOM / 10)
 echo "Sleeping for $SLEEP_FOR seconds"
-date
+date | tee ~/.sv-cron-run-all-upgrades-last
 sleep $SLEEP_FOR
 echo "Proceeding with upgrades"
 date
 
 cd ~/sv-parity-setup
 git pull
-./bin/run_all_upgrades.sh
+./bin/run_all_upgrades.sh | tee -a ~/.sv-cron-run-all-upgrades-last
