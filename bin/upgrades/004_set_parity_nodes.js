5f8c5cd4251d992adaeb107cfea2424727d818d4
Max Kaye <m@xk.io>
2018-04-13 10:11:11 +1000

    restart parity after config upgrade

--- bin/upgrades/004_set_parity_nodes.js
+++ bin/upgrades/004_set_parity_nodes.js
@@ -4,5 +4,6 @@ require('./upgradeLib')("004-set-parity-peers", function(){
         c['network']['min_peers'] = 50
         c['network']['max_peers'] = 500
     })
+    execCmd("systemctl restart parity")
 
 });
