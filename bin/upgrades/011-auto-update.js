b75df8f5337783e2fdc5a632f447c7c12a245bf4
Max Kaye <m@xk.io>
2018-06-06 16:58:14 +1000

    fix autoupdate

--- bin/upgrades/011-auto-update.js
+++ bin/upgrades/011-auto-update.js
@@ -2,7 +2,8 @@ require('./upgradeLib')("011-auto-update", function(){
 
     editParityConfig(c => {
         c['parity']['auto_update'] = "all" 
-        c['parity']['auto_update_delay'] = 2000
+        // this is in the parity config generator but apparently doesn't exist!
+        // c['parity']['auto_update_delay'] = 2000
         c['parity']['release_track'] = "beta"
     })
     execCmd("systemctl restart parity")
