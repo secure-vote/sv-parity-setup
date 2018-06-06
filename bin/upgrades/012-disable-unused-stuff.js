470f58d6f3746327561ba06f2d56786aaf528a91
Max Kaye <m@xk.io>
2018-06-06 18:03:37 +1000

    proxy fix worked - roll out no-persistent-txqueu

--- bin/upgrades/012-disable-unused-stuff.js
+++ bin/upgrades/012-disable-unused-stuff.js
@@ -8,6 +8,7 @@ require('./upgradeLib')("012-disable-unused", function(){
         c['rpc']['server_threads'] = 15
         c['rpc']['processing_threads'] = 15
         c['mining']['tx_queue_size'] = 42949672
+        c['parity']['no_persistent_txqueue'] = true
     })
     restartParity()
 
