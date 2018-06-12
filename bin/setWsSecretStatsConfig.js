3a6e99d226caabad7f8201bd3ff8a320c61ba7f9
Max Kaye <m@xk.io>
2018-06-12 15:35:38 +1000

    add name param to setWsSecretStats

--- bin/setWsSecretStatsConfig.js
+++ bin/setWsSecretStatsConfig.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 
 let filename;
 let secret;
+let name
 for (var i = 0; i < process.argv.length; i++){
     let arg = process.argv[i];
     if (arg.includes('=')) {
@@ -15,13 +16,16 @@ for (var i = 0; i < process.argv.length; i++){
             case "secret":
                 secret = value
                 break
+            case "name":
+                name = value
+                break
         }
     }
 }
 
-if (filename === undefined || secret === undefined) {
+if (filename === undefined || secret === undefined || name === undefined) {
     console.log(`Usage:
-    node ./setWsSecretStatsConfig.js filename=/home/ubuntu/eth-stats/someApp.json secret=yourSecretHere
+    node ./setWsSecretStatsConfig.js filename=/home/ubuntu/eth-stats/someApp.json secret=yourSecretHere "name=My Node Name"
 
     filename must be the full path
 
