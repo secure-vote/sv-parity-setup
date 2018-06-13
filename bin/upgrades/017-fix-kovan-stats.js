ca2bb03739739ed6f8f32b6e7adc93756aeb880c
Max Kaye <m@xk.io>
2018-06-13 10:38:07 +1000

    add upgrade to fix kovan stats

---
+++ bin/upgrades/017-fix-kovan-stats.js
@@ -0,0 +1,16 @@
+require('./upgradeLib')("FILENAME", function(){
+
+    const ethStatsDir = "/home/ubuntu/eth-stats"
+    const configFilename = "app-parity-stats.json"
+    const configPath = `${ethStatsDir}/${configFilename}`
+
+    editJsonFile(configPath, config => {
+        const s = config[0].env.WS_SERVER
+        if (s === "wss://stats-kovan.parity.io") {
+            config[0].env.WS_SERVER = `wss://kovan-stats.parity.io`
+        }
+    })
+
+    execCmd("pm2 delete stats-parity")
+    execCmd(`cd /home/ubuntu/eth-stats/ && pm2 start ${configFilename} && pm2 save`)
+});
