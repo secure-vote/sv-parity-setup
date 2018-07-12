4c543dbe9976f21288f71fc9d134a70cd1d33795
Max Kaye <m@xk.io>
2018-07-12 23:08:41 +1000

    Add debug logging

---
+++ bin/upgrades/022-add-debug-log.js
@@ -0,0 +1,7 @@
+require('./upgradeLib')("FILENAME", function(){
+
+    execCmd('cd /home/ubuntu/sv-parity-setup && ./installServices.sh', true)
+    execCmd('sudo systemctl daemon-reload', true)
+    execCmd('sudo systemctl restart parity', true)
+
+});
