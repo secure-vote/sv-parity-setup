732dd819543a594be9e2beab94ea495d3aebd6e1
Max Kaye <m@xk.io>
2018-07-17 09:12:06 +1000

    Improve log transition slightly

--- bin/upgrades/023-fix-debug-logging.3.js
+++ bin/upgrades/023-fix-debug-logging.3.js
@@ -9,7 +9,8 @@ require('./upgradeLib')("FILENAME", function(){
     execCmd('sudo systemctl restart parity', true)
 
     // make sure we move the syslog b/c it's probs like 2.5GB
-    execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup')
-    execCmd('sudo -u syslog touch /var/log/syslog && sudo chown syslog:adm /var/log/syslog')
+    execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup.3')
+    execCmd('sudo -u syslog touch /var/log/syslog && sudo chown syslog:adm /var/log/syslog && sudo chmod 640 /var/log/syslog')
+    execCmd('sudo systemctl restart syslog')
 
 });
