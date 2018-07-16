0095b8c28a7e2f90bd1c75a297b0cdff9d3da095
Max Kaye <m@xk.io>
2018-07-17 09:03:27 +1000

    add fix for logging clobbering

---
+++ bin/upgrades/023-fix-debug-logging.js
@@ -0,0 +1,14 @@
+require('./upgradeLib')("FILENAME", function(){
+
+    // pm2 restart every hour
+    updateCron()
+
+    // need to set up new service w/ logfile defined as arg
+    execCmd('cd /home/ubuntu/sv-parity-setup && ./installServices.sh', true)
+    execCmd('sudo systemctl daemon-reload', true)
+    execCmd('sudo systemctl restart parity', true)
+
+    // make sure we move the syslog b/c it's probs like 2.5GB
+    execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup')
+
+});
