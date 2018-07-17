2d31bbfc626db44e7f778ee08bd69e54bf759f29
Max Kaye <m@xk.io>
2018-07-17 10:58:16 +1000

    do syslog.1 too

--- bin/upgrades/023-fix-debug-logging.7.js
+++ bin/upgrades/023-fix-debug-logging.7.js
@@ -9,6 +9,7 @@ require('./upgradeLib')("FILENAME", function(){
     execCmd('sudo systemctl restart parity', true)
 
     // make sure we move the syslog b/c it's probs like 2.5GB
+    execCmd('sudo mv /var/log/syslog.1 /mnt/eth/syslog.1.backup.7')
     execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup.7')
     execCmd('sudo -u syslog touch /var/log/syslog && sudo chown syslog:adm /var/log/syslog')
     execCmd('sudo systemctl restart syslog')
