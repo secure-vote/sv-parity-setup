6d215b4972a07e27b3aa8a897326214b44055e8b
Max Kaye <m@xk.io>
2018-07-17 10:57:07 +1000

    add back touch of syslog file

--- bin/upgrades/023-fix-debug-logging.7.js
+++ bin/upgrades/023-fix-debug-logging.7.js
@@ -9,7 +9,8 @@ require('./upgradeLib')("FILENAME", function(){
     execCmd('sudo systemctl restart parity', true)
 
     // make sure we move the syslog b/c it's probs like 2.5GB
-    execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup.6')
+    execCmd('sudo mv /var/log/syslog /mnt/eth/syslog.backup.7')
+    execCmd('sudo -u syslog touch /var/log/syslog && sudo chown syslog:adm /var/log/syslog')
     execCmd('sudo systemctl restart syslog')
 
 });
