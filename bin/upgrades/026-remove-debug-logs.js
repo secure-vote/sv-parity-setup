e751c8f967738a51f01c8fedc04a03177fb0d556
Max Kaye <m@xk.io>
2018-08-20 14:14:59 +1000

    fix upgrade for debug log

--- bin/upgrades/026-remove-debug-logs.js
+++ bin/upgrades/026-remove-debug-logs.js
@@ -3,7 +3,7 @@ require('./upgradeLib')("FILENAME", function(){
     // debug logs are super massive, like 20GB after a few days    O.O
     execCmd('cd /home/ubuntu/sv-parity-setup && ./installServices.sh', true)
     execCmd('sudo systemctl daemon-reload', true)
-    execCmd('sudo systemctl stop parity && rm /mnt/eth/debug.log', true)
+    execCmd('sudo systemctl stop parity && rm -f /mnt/eth/debug.log', true)
     execCmd('sudo systemctl start parity', true)
     execCmd('pm2 restart all')
 
