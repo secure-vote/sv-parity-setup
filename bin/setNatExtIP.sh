85a97133d1fc11257fe996f6e6a30150086af0b3
Max Kaye <m@xk.io>
2018-08-21 13:18:17 +1000

    fix up setNatExtIP

---
+++ bin/setNatExtIP.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source ~/bin/_loadCommon.sh
+
+read -p "What is this nodes external IP address? > " EXTIP
+
+echo "Configuring for IP $EXTIP"
+
+cd ~/bin/
+node _setNatExtIP.js "$EXTIP"
