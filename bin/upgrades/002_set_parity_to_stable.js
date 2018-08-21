6cfc52ca23a5f86b5d65c48f32d00d14ae6f3f08
Max Kaye <m@xk.io>
2018-08-21 14:15:28 +1000

    remove bad upgrades and add bootnotes to stopgap-spec

--- bin/upgrades/002_set_parity_to_stable.js
+++ bin/upgrades/002_set_parity_to_stable.js
@@ -1,22 +1,24 @@
 require('./upgradeLib')("002", function(){
 
-    execCmd("cp ~/.local/share/io.parity.ethereum/config.toml ~/.parity-config-backup-002")
+    // don't need to do this anymore
 
-    const c = toml.parse(fs.readFileSync(parityConfigPath));
-    if (c['parity'] === undefined)
-        fatalError("Parity config seems malformed")
-    c['parity']['release_track'] = "stable"
-    c['rpc']['hosts'] = ['*']
-    fs.writeFileSync(parityConfigPath, toml.dump(c))
-    // python toml ends lists like ["item","thing",] -- nodejs toml doesn't like it
-    execCmd("sed -i 's/, undefined//' ~/.local/share/io.parity.ethereum/config.toml")
-    execCmd("sed -i 's/,]/]/' ~/.local/share/io.parity.ethereum/config.toml")
-    execCmd("sed -i 's/, ]/]/' ~/.local/share/io.parity.ethereum/config.toml")
+    // execCmd("cp ~/.local/share/io.parity.ethereum/config.toml ~/.parity-config-backup-002")
 
-    execCmd("ln -s /mnt/eth ~/.parity")
+    // const c = toml.parse(fs.readFileSync(parityConfigPath));
+    // if (c['parity'] === undefined)
+    //     fatalError("Parity config seems malformed")
+    // c['parity']['release_track'] = "stable"
+    // c['rpc']['hosts'] = ['*']
+    // fs.writeFileSync(parityConfigPath, toml.dump(c))
+    // // python toml ends lists like ["item","thing",] -- nodejs toml doesn't like it
+    // execCmd("sed -i 's/, undefined//' ~/.local/share/io.parity.ethereum/config.toml")
+    // execCmd("sed -i 's/,]/]/' ~/.local/share/io.parity.ethereum/config.toml")
+    // execCmd("sed -i 's/, ]/]/' ~/.local/share/io.parity.ethereum/config.toml")
 
-    execCmd("systemctl stop parity")
-    execCmd("rm -rf ~/.local/share/io.parity.ethereum-updates/");
-    execCmd("systemctl start parity")
+    // execCmd("ln -s /mnt/eth ~/.parity")
+
+    // execCmd("systemctl stop parity")
+    // execCmd("rm -rf ~/.local/share/io.parity.ethereum-updates/");
+    // execCmd("systemctl start parity")
 
 });
--- bin/upgrades/013-fix-params-and-move-to-stable.js
+++ bin/upgrades/013-fix-params-and-move-to-stable.js
@@ -5,11 +5,12 @@ require('./upgradeLib')("012-fix-params-be-stable", function(){
         c['mining']['tx_queue_size'] = 10000
         c['parity']['release_track'] = "stable"
     })
-    execCmd("rm -rf /home/ubuntu/.local/share/io.parity.ethereum-updates/")
-    downloadTmpFileAndRunF("https://d1h4xl4cr1h0mo.cloudfront.net/v1.10.6/x86_64-unknown-linux-gnu/parity_1.10.6_ubuntu_amd64.deb", filePath => {
-        assertSha256Checksum(filePath, "7f49be8aaf88d31750c43b5cd6787bbab43bdb970a72b3a6b3af3eb69eda0108")
-        execCmd(`dpkg -i ${filePath}`)
-    })
+    // don't do this for any new nodes
+    // execCmd("rm -rf /home/ubuntu/.local/share/io.parity.ethereum-updates/")
+    // downloadTmpFileAndRunF("https://d1h4xl4cr1h0mo.cloudfront.net/v1.10.6/x86_64-unknown-linux-gnu/parity_1.10.6_ubuntu_amd64.deb", filePath => {
+    //     assertSha256Checksum(filePath, "7f49be8aaf88d31750c43b5cd6787bbab43bdb970a72b3a6b3af3eb69eda0108")
+    //     execCmd(`dpkg -i ${filePath}`)
+    // })
     restartParity()
 
 });
